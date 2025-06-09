3a6467823acde175bf14ce0f1600a495be83fdbb
AlinaPerf <alinaperfileva44@gmail.com>
2025-06-09 19:46:40 +0300

    Add files via upload

---
+++ 26.05.2025.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+   /* int number = 4;
+    A = number;*/
+    //int number;
+    //std::cin >> number;
+    //if (number % 2 == 0) {
+    //    std::cout << number <<" " << "true" << "\n";
+    //}
+    //else
+    //{
+    //    std::cout << number <<" " << "false" << "\n";
+    //}
+    //return 0;
+    int mass [10];
+    int min = 0;
+    int max = 100;
+
+for (int i = 0; i < 10; ++i) {
+    mass[i] = rand() % max;
+    std::cout << mass[i] << "\n ";
+}
+
+
+
+return 0;
+}
+
+
---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+
+//void Koktel(std::vector<int> &bob) {
+//	bool change = true;
+//	while (change) {
+//		change = false;
+//		for (int i = 0; i < bob.size() - 1; i++) {
+//			if (bob[i] > bob[i + 1]) {
+//				int c = bob[i];
+//				bob[i] = bob[i + 1];
+//				bob[i + 1] = c;
+//
+//				change = true;
+//			}
+//		}
+//		for (int j = bob.size() - 1; j > 0; j--) {
+//			if (bob[j] < bob[j - 1]) {
+//				int d = bob[j];
+//				bob[j] = bob[j - 1];
+//				bob[j - 1] = d;
+//
+//				change = true;
+//			}
+//		}
+//
+//	}
+//}
+
+
+
+int main() {
+
+	std::string number = "458 22 00 0 29 7979";
+
+	
+
+	while () {
+
+
+	for (int i = 0; i < number.size(); i++){
+		std::cout << number[i] << "\n";
+	}
+	}
+	//std::vector<int> bob = {87, 4, 0, 56, 11, 3, 5, 5, 68};
+	//Koktel(bob);
+	// 
+	// Все что ниже одно и то же
+	//for (int i : bob) {
+	//	std::cout << i << " ";
+	//}
+	/*for (int i = 0; i < bob.size(); i++{
+		std::cout << bob[i] << " ";
+	}*/
+}
+
